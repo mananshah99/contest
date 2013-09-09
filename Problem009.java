4a49634f19b6ab96a81af23143b2e68dd614c9e1
Manan Shah <manan.shah.777@gmail.com>
2013-09-09 07:17:08 -0700

    Create Problem009.java

---
+++ Problem009.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecteuler;
+
+/**
+ *
+ * @author Manan
+ */
+public class Problem009 {
+    public static void euler009() 
+    {
+    }
+}
