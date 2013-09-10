a7c13bc7f9dfd417053ee146f27d08ce941a5ab9
Manan Shah <manan.shah.777@gmail.com>
2013-09-10 07:02:34 -0700

    Added problem 12 solution

--- Problem012.java
+++ Problem012.java
@@ -9,8 +9,31 @@ package projecteuler;
  * @author Manan
  */
 public class Problem012 {
+    int i=1;
+    int numfactors = 0;
     public static void euler012()
     {
-    
+        while(getNumFactors(nextTriangularNumber()) !=500)
+        {
+            if(getNumFactors(nextTriangularNumber())==500) return i;
+            numfactors = 0;
+        }
+        return i;
+    }
+    public int nextTriangularNumber()
+    {
+         for(int j=0; j<i; j++)
+         {
+             i+=j;
+         }
+         return i;
+    }
+    public int getNumFactors(int num)
+    {
+        for(int k=0; k<=(num/2); k++)
+        {
+            numfactors++;
+        }
+        return numfactors;
     }
 }
