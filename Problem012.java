a4af0cbdf3e584925d408972697eb1fdbaf5fbc0
Manan Shah <manan.shah.777@gmail.com>
2013-09-09 22:38:34 -0700

    Create Problem012.java

---
+++ Problem012.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecteuler;
+
+/**
+ *
+ * @author Manan
+ */
+public class Problem012 {
+    public static void euler012()
+    {
+    
+    }
+}
