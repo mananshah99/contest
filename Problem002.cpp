8c20dfa3174eb9f62131599164f32119c358fba9
Manan Shah <manan.shah.777@gmail.com>
2013-11-01 23:37:26 -0700

    Added C++ source for Problem 2

---
+++ Problem002.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int x= 1;
+    int y=1;
+    int z = 0;
+    int result = 0;
+    
+    while(z<4000000)
+    {
+        z = (x+y);
+       if(z%2==0) result += z;\
+       
+       //next iteration
+       
+       x = y;
+       y = z;
+    }
+    
+    cout<<result<<endl;
+}
