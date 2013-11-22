eba9103a1015d81f6f393e4b6d3442c783d1f9c9
Manan Shah <manan.shah.777@gmail.com>
2013-11-22 15:44:20 -0800

    Update Problem031.java
    Fixed code for readability

--- Problem031.java
+++ Problem031.java
@@ -9,22 +9,21 @@ package projecteuler;
  * @author Manan
  */
 public class Problem031 {
-    //terrible running time - will fix asap
+    //terrible running time - will fix 
     public static void euler031()
     {
         int count = 1;
         int sum;
-        for(int a = 0; a<=200;a++){
-            for(int b = 0; b<=100;b++){
-                for(int c = 0; c<=40;c++){
-                    for(int d = 0;d<=20;d++){
-                        for(int e = 0; e<=10;e++){
-                            for(int f = 0; f<=4;f++){
-                                for(int g = 0; g<=2;g++){
+        for(int a = 0; a<=200; a++){
+            for(int b = 0; b<=100; b++){
+                for(int c = 0; c<=40; c++){
+                    for(int d = 0; d<=20; d++){
+                        for(int e = 0; e<=10; e++){
+                            for(int f = 0; f<=4; f++){
+                                for(int g = 0; g<=2; g++){
                                     sum = a*1+b*2+c*5+d*10+e*20+f*50+g*100;
-                                    if(sum==200){
+                                    if(sum==200)
                                         count++;
-                                    }
                                 }
                             }
                         }
