e1b6f3e04b0ffb92def7585c2b03e15c2a1b8585
Manan Shah <manan.shah.777@gmail.com>
2013-11-21 19:29:21 -0800

    Update Problem014.java
    Cleaned up code

--- Problem014.java
+++ Problem014.java
@@ -20,14 +20,8 @@ public class Problem014 {
             
             while(num > 1)
             {
-                if(num % 2 == 0)
-                {
-                    num = num / 2;
-                } 
-                else
-                {
-                    num = (num * 3) + 1;
-                }
+                if(num % 2 == 0) num = num / 2;
+                else num = (num * 3) + 1;
                 count ++;
             }
              
