8181a25efd1ea62e4af09bb5846400441195fdd8
Manan Shah <manan.shah.777@gmail.com>
2014-02-04 11:14:19 -0800

    Added P019 in C#
    The DateTime library is extremely useful

---
+++ Problem019.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Problem019
+{
+	public static void Main()
+	{
+		int sundays = 0;
+        for (int year = 1901; year <= 2000; year++) 
+		{
+            for (int month = 1; month <= 12; month++) 
+			{
+				DateTime t = new DateTime(year, month, 1);
+                if (t.DayOfWeek == DayOfWeek.Sunday)
+                    sundays++;
+            }
+        }
+        Console.WriteLine(sundays);
+    }
+}
