5c074f21a3ea3b82226e49a6b44bd72bc3096ffa
Manan Shah <manan.shah.777@gmail.com>
2014-02-04 21:27:55 -0800

    Update Problem019.cs
    Readability and code tweaks

--- Problem019.cs
+++ Problem019.cs
@@ -1,19 +1,18 @@
 using System;
 
+//uses the DateTime library in C# for computing Sundays
 public class Problem019
 {
-	public static void Main()
+	public static void Main() 
 	{
 		int sundays = 0;
-        for (int year = 1901; year <= 2000; year++) 
-		{
-            for (int month = 1; month <= 12; month++) 
-			{
-				DateTime t = new DateTime(year, month, 1);
-                if (t.DayOfWeek == DayOfWeek.Sunday)
-                    sundays++;
-            }
-        }
-        Console.WriteLine(sundays);
-    }
+        	for (int year = 1901; year <= 2000; year++){
+            		for (int month = 1; month <= 12; month++) {
+		        	DateTime t = new DateTime(year, month, 1);
+                		if (t.DayOfWeek == DayOfWeek.Sunday)
+                    			sundays++;
+            		}
+        	}
+        	Console.WriteLine(sundays);
+    	}
 }
