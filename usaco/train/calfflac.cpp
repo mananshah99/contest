f261776f15d32c51a59b8bea649d8eb3f6cece7b
Manan Shah <manan.shah.777@gmail.com>
2015-03-24 22:14:24 -0700

    Update calfflac.cpp

--- usaco/train/calfflac.cpp
+++ usaco/train/calfflac.cpp
@@ -29,59 +29,6 @@
 #include <complex>
 using namespace std;
 
-/*
-int main() {
-	//freopen("title.in", "r", stdin);
-	//freopen("title.out", "w", stdout);
-
-	string s; cin >> s;
-	string best = "";
-	int longest = 1;
-	
-	for (auto it = s.begin(); it != s.end(); ++it) {
-		cout << "iterator -> " << *it << endl;
-		auto start = it;
-		auto end = it;
-
-		int size;
-		if (isalpha(*it)) size = 1;
-		else size = 0;
-
-		while (true) {
-			bool done = false;
-			//go as far back as possible with punctuation
-			do {
-				if (start == s.begin()) break;
-				--start;
-				done = done || (start <= s.begin());
-			} while (!done && !isalpha(*start));
-
-			//go as far forward as possible without punctuation
-			do {
-				if (end == s.end()) break;
-				++end;
-				done = done || (end >= s.end());
-			} while (!done && !isalpha(*end));
-			
-			//if we're done or the resulting string isn't a palindrome, break
-			if(!done) cout << "two characters (this is important) : " << *start << " " << *end  << endl;
-			if (done || tolower(*start) != tolower(*end)) break;
-			//add the two (alphabet) characters to the size
-			size += 2;
-
-			if (size > longest) {
-				//add characters to the string
-				best = string(start, end + 1);
-				cout << "changed best. It's now : " << best << endl;
-				longest = size;
-			}
-		}
-	}
-	cout << longest << endl << best << endl;
-	pause();
-	return 0;
-}*/
-
 int main() {
 	string s;
 	cin >> s;
